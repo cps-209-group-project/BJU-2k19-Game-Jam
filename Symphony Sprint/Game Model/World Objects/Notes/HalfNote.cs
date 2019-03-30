0c0ef4bdf5e12609e486945d10c8dd8856f99d2f
scham978 <scham978@bju.edu>
2019-03-30 15:49:06 -0400

    Added new game objects

---
+++ Symphony Sprint/Game Model/World Objects/Notes/HalfNote.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symphony_Sprint.Game_Model.World_Objects.Notes
+{
+    class HalfNote : GameObject
+    {
+
+        public int pointValue;
+        public override string ImgPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int Speed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int PosX { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int PosY { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
