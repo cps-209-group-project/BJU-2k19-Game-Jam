0055ed0a88fba1c75699b5cca575977f040edc80
amora479 <amora479@students.bju.edu>
2019-04-08 02:31:53 -0400

    added title screen and paired with the game window

--- Symphony Sprint/MainWindow.xaml.cs
+++ Symphony Sprint/MainWindow.xaml.cs
@@ -24,6 +24,10 @@ namespace Symphony_Sprint
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
             this.KeyDown += new KeyEventHandler(GameController.Instance.Player.KeyIsDown);
             this.KeyUp += new KeyEventHandler(GameController.Instance.Player.KeyIsUp);
             GameWindow gwin = new GameWindow();
